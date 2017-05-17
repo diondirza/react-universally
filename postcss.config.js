7e012b67bf1c67fd5d9391ceaf1571e2cf248232
Dion Dirza <dion.dirza@gmail.com>
2017-05-17 21:31:45 +0700

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
+
+const reactToolboxVariables = {
+  'color-primary': '#07addc',
+};
+
 // PostCSS-Loader config options
 module.exports = {
-  plugins: [require('postcss-import'), require('postcss-simple-vars'), require('postcss-cssnext')],
+  plugins: [
+    require('postcss-cssnext')({
+      features: {
+        customProperties: {
+          variables: reactToolboxVariables,
+        },
+      },
+    }),
+    require('postcss-modules-values'),
+  ],
 };
